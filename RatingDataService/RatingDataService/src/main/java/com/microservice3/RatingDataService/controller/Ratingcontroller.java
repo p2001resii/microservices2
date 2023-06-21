3770ce953f7e078dabfa88dd1636c6846b449a02
prasanna <youexample.com>
2023-06-21 09:57:16 +0530

    serverupdateone

--- RatingDataService/RatingDataService/src/main/java/com/microservice3/RatingDataService/controller/Ratingcontroller.java
+++ RatingDataService/RatingDataService/src/main/java/com/microservice3/RatingDataService/controller/Ratingcontroller.java
@@ -1,17 +1,29 @@
 package com.microservice3.RatingDataService.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice3.RatingDataService.entity.Rating;
+import com.microservice3.RatingDataService.entity.UserRating;
 
-@RequestMapping
+@RestController
 public class Ratingcontroller {
 
-	@GetMapping("/rating/{userId}")
+	@GetMapping("/ratingdata/{userId}")
 	public Rating getRating(@PathVariable String userId) {
 		return new Rating("M111",5);
 	}
+	@GetMapping("/ratingdata/users/{userId}")
+	public UserRating getUserRating(@PathVariable String userId) {
+		List<Rating> ratingsList = Arrays.asList(new Rating("1234",4),new Rating("2345",5));
+		
+	UserRating ur =new UserRating();
+     ur.setUserRating(ratingsList);
+	return ur;
 
+}
 }
--- movieCatalogService/movieCatalogService/src/main/java/com/microservice2/movieCatalogService/controller/CatalogController.java
+++ movieCatalogService/movieCatalogService/src/main/java/com/microservice2/movieCatalogService/controller/CatalogController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,22 +13,24 @@ import org.springframework.web.client.RestTemplate;
 import com.microservice2.movieCatalogService.entity.Catalog;
 import com.microservice2.movieCatalogService.entity.Movie;
 import com.microservice2.movieCatalogService.entity.Rating;
+import com.microservice2.movieCatalogService.entity.UserRating;
 
 @RestController
 public class CatalogController {
+	@Autowired
+	private RestTemplate rt;
 
 	@GetMapping("/catalog/{userId}")
 	public List <Catalog> getcatlog(@PathVariable String userId){
 		
-		//return Collections.singletonList(new Catalog("KGF","coal mining",4));
+//		return Collections.singletonList(new Catalog("KGF","coal mining",4));
 		
-		
-		List<Rating> ratingsList = Arrays.asList(new Rating("444",4),new Rating("44",5));
+//		List<Rating> ratingsList = Arrays.asList(new Rating("444",4),new Rating("44",5));
+		  UserRating UserRating = rt.getForObject("http://localhost:8081/ratingdata/users/"+userId,UserRating.class);
 		 
-		RestTemplate rt = new RestTemplate();
-		Movie	m = rt.getForObject("http://localhost:8080/movie/1234", Movie.class);
-		return ratingsList.stream().map(rating ->{
-			return new  Catalog("VIP","vengence",rating.getRating());
+		return UserRating.getUserRating().stream().map(rating ->{
+		Movie	m = rt.getForObject("http://localhost:8080/movie/"+rating.getMovieid(), Movie.class);
+			return new  Catalog(m.getName(),"abcds",rating.getRating());
 		}).collect(Collectors.toList());
 
 }
